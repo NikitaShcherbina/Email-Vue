4c8732174e6e9ff92fdf29969cefe13d3fed7127
Nikita <Nikita.Shcherbina97@gmail.com>
2020-10-30 12:29:29 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
   justify-content: center;
   align-items: center;
   .container {
-    max-width: 1280px;
+    width: 1280px;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
